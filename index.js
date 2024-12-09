9a4d63f65ec714cca48a68fb5fa160fd3bc59811
Sebas <sebaslg96@gmail.com>
2024-12-09 12:21:56 -0500

    arreglada variables de entorno

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require("cors")
 
 const app = express()
-require('dotnet').config()
+require('dotenv').config()
 
 app.use(cors())
 
--- src/bd.js
+++ src/bd.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2')
 
-const cnx = mysql.createConnection({
+ const cnx = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     database: process.env.DB_NAME,
-});
+    password: process.env.DB_PASSWORD
+}); 
+/* const cnx = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'interpolice',
+}); */
 
 cnx.connect((error)=>{
     if (error) {
